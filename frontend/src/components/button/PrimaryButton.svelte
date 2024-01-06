0aa5b00096c8ee2f0f7b31e75796f155370d7a48
Kaj Rusilowski <xedom08@gmail.com>
2024-01-06 17:58:33 +0100

    fix duplicate children

--- frontend/src/components/button/PrimaryButton.svelte
+++ frontend/src/components/button/PrimaryButton.svelte
@@ -2,9 +2,12 @@
 	import type { ComponentProps } from 'svelte';
 	import Button from './Button.svelte';
 
-	let { text, ...props } = $props<ComponentProps<Button> & { text: string }>();
+	let { text, children, ...props } = $props<ComponentProps<Button> & { text: string }>();
 </script>
 
-<Button class="flex items-center justify-center px-4 py-2 bg-orange-400 w-fit h-fit" {...props}>
+<Button
+	class="flex items-center justify-center px-4 py-2 bg-orange-400 rounded w-fit h-fit"
+	{...props}
+>
 	{text}
 </Button>
