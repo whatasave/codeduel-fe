a8bc5752fbd3520998b8391f907e71b1661abf21
Michi Palazzo <=>
2024-01-06 16:54:24 +0100

    Update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import Button from '../components/button/Button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button onclick={() => console.log('ciao')}>
+	{#snippet children()}{/snippet}
+</Button>
+<Button onclick={() => console.log('ciao')}>ciao</Button>
