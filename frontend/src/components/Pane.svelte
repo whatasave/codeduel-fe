33e3df3de9bcad636203ca5e047914d0bc4a7aa9
Michi Palazzo <=>
2024-01-15 20:18:26 +0100

    Update

---
+++ frontend/src/components/Pane.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import clsx from 'clsx';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let { class: className, children } = $props<HTMLAttributes<HTMLDivElement>>();
+</script>
+
+<div class={clsx('bg-[#151515] rounded', className)}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
--- frontend/src/routes/match/[id=integer]/+page.svelte
+++ frontend/src/routes/match/[id=integer]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Pane from '$components/Pane.svelte';
+
 	const users = $state([
 		{ id: 1, name: 'John' },
 		{ id: 2, name: 'Jane' },
@@ -7,13 +9,13 @@
 </script>
 
 <div class="flex h-full p-2 gap-2">
-	<div class="flex flex-col w-20 h-full bg-[#151515] rounded"></div>
+	<Pane class="flex flex-col w-20 h-full"></Pane>
 	<div class="flex flex-col flex-[0.8_0.8_0%] gap-2">
-		<div class="flex flex-col flex-1 bg-[#151515] rounded"></div>
-		<div class="flex flex-col h-20 bg-[#151515] rounded"></div>
+		<Pane class="flex flex-col flex-1"></Pane>
+		<Pane class="flex flex-col h-20"></Pane>
 	</div>
 	<div class="flex flex-col flex-1 gap-2">
-		<div class="flex flex-col flex-1 bg-[#151515] rounded"></div>
-		<div class="flex flex-col h-80 bg-[#151515] rounded"></div>
+		<Pane class="flex flex-col flex-1"></Pane>
+		<Pane class="flex flex-col h-80"></Pane>
 	</div>
 </div>
