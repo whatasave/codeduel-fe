2e26ae5a9b241ca24f994b9af622bf19c369903c
Kaj Rusilowski <xedom08@gmail.com>
2024-02-07 02:03:21 +0100

    update lobbies

--- src/routes/(layout)/lobby/+page.svelte
+++ src/routes/(layout)/lobby/+page.svelte
@@ -4,10 +4,20 @@
 
 	type LobbyType = {
 		id: string;
-		owner: string;
-		players: string[];
-		max_players: number;
 		state: { type: string };
+		max_players: number;
+		playerCount: number;
+		owner: User;
+		users: Record<string, User>;
+	};
+
+	type User = {
+		id: 1;
+		avatar: string;
+		email: string;
+		expires_at: string;
+		token: string;
+		username: string;
 	};
 
 	let lobbies = $state<LobbyType[]>([]);
@@ -17,6 +27,12 @@
 		const data = await res.json();
 
 		lobbies = data as LobbyType[];
+		lobbies = lobbies.map((lobby) => ({
+			...lobby,
+			playerCount: Object.keys(lobby.users).length
+		}));
+
+		console.log(lobbies);
 	});
 </script>
 
@@ -28,17 +44,29 @@
 				<Button text="Create" variant="primary" />
 			</a>
 		</div>
-
 		{#each lobbies as lobby}
 			<div class="bg-[#050505] rounded py-2 px-4 flex gap-2 items-center">
+				<div>
+					{#each Object.values(lobby.users) as user}
+						<img class="w-8 h-8 rounded-full" src={user.avatar} alt={user.username} />
+					{/each}
+				</div>
 				<div class="flex flex-col flex-1">
-					<p class="text-2xl font-semibold">{lobby.owner}'s lobby</p>
-					<p>{lobby.state.type} - Locked - {lobby.players}/{lobby.max_players} Brogrammers</p>
+					<p class="text-2xl font-semibold">{lobby.owner.username}'s lobby</p>
+					<p>
+						{lobby.state.type} | Locked | {lobby.playerCount}/{lobby.max_players} Brogrammers
+					</p>
 				</div>
 				<div class="flex gap-2">
-					<a href={`/lobby/${lobby.id}`}>
-						<Button text="Join" />
-					</a>
+					{#if lobby.state.type === 'game'}
+						<a href={`/match/${lobby.id}`}>
+							<Button text="Re-Join" class="text-sky-500" />
+						</a>
+					{:else}
+						<a href={`/lobby/${lobby.id}`}>
+							<Button text="Join" class="text-green-500" />
+						</a>
+					{/if}
 					<!-- TODO use ButtonIcon -->
 				</div>
 			</div>
