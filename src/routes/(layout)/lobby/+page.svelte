5ff09dd795fa54aefdb74e85b19317136df57e28
Michi Palazzo <palazzomichi@gmail.com>
2024-02-07 01:29:14 +0100

    Update

--- src/routes/(layout)/lobby/+page.svelte
+++ src/routes/(layout)/lobby/+page.svelte
@@ -3,21 +3,21 @@
 	import { onMount } from 'svelte';
 
 	type LobbyType = {
-        "id": string;
-        "owner": string;
-        "players": string[];
-        "max_players": number;
-        "state": string;
-	}
+		id: string;
+		owner: string;
+		players: string[];
+		max_players: number;
+		state: { type: string };
+	};
 
 	let lobbies = $state<LobbyType[]>([]);
 
-    onMount(async () => {
-        const res = await fetch("http://localhost:8080/lobbies")
-        const data = await res.json()
+	onMount(async () => {
+		const res = await fetch('http://localhost:8080/lobbies');
+		const data = await res.json();
 
-		lobbies = data as LobbyType[]
-    })
+		lobbies = data as LobbyType[];
+	});
 </script>
 
 <div class="flex gap-2 w-full max-w-[60rem] max-h-[70vh] overflow-y-auto">
@@ -33,12 +33,12 @@
 			<div class="bg-[#050505] rounded py-2 px-4 flex gap-2 items-center">
 				<div class="flex flex-col flex-1">
 					<p class="text-2xl font-semibold">{lobby.owner}'s lobby</p>
-					<p>{lobby.state} - Locked - {lobby.players}/{lobby.max_players} Brogrammers</p>
+					<p>{lobby.state.type} - Locked - {lobby.players}/{lobby.max_players} Brogrammers</p>
 				</div>
 				<div class="flex gap-2">
-                    <a href={`/lobby/${lobby.id}`}>
-				        <Button text="Join" />
-                    </a>
+					<a href={`/lobby/${lobby.id}`}>
+						<Button text="Join" />
+					</a>
 					<!-- TODO use ButtonIcon -->
 				</div>
 			</div>
