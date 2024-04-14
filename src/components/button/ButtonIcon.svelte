6f88ed444d3571a6bdbc2c2d6f34feafed3e07d1
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 16:42:46 +0200

    added styles to button icon

--- src/components/button/ButtonIcon.svelte
+++ src/components/button/ButtonIcon.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
 	import type { ComponentProps, ComponentType, SvelteComponent } from 'svelte';
 	import ButtonLike from './ButtonLike.svelte';
+	import { cva, type VariantProps } from 'class-variance-authority';
 
-	type $$Props = Omit<ComponentProps<ButtonLike>, 'children'> & {
+	const classes = cva('w-fit h-fit disabled:opacity-60 disabled:cursor-not-allowed', {
+		variants: {
+			variant: {
+				primary: 'px-6 py-1 bg-[#151515] text-white rounded-sm font-semibold',
+				accent: 'px-6 py-1 bg-[#ECC206] rounded-sm text-black font-semibold',
+				empty: 'px-6 py-1 border-2 text-white border-white rounded-sm font-semibold',
+				danger: 'px-6 py-1 bg-red-500 text-white rounded-sm font-semibold'
+			}
+		}
+	});
+
+	interface $$Props extends Omit<ComponentProps<ButtonLike>, 'children'>, VariantProps<typeof classes> {
 		text?: string;
+		iconClass?: string;
+		iconAlign?: 'left' | 'right';
 		icon: ComponentType<SvelteComponent>;
-	};
+	}
 
-	let { text, class: className, icon, ...props }: $$Props = $props();
+	let { text, class: className, variant, icon, iconClass, iconAlign = 'right', ...props }: $$Props = $props();
 </script>
 
-<ButtonLike {...props}>
+<ButtonLike {...props} class={classes({ variant, className })}>
 	<div class="flex items-center gap-2">
+		{#if iconAlign === 'left'}
+			<svelte:component this={icon} class={iconClass} />
+		{/if}
 		{#if text}
 			<span>{text}</span>
 		{/if}
-		<svelte:component this={icon} />
+		{#if iconAlign === 'right'}
+			<svelte:component this={icon} class={iconClass} />
+		{/if}
 	</div>
 </ButtonLike>
