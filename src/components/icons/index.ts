d9fe67185ca32b5e12d5e6a53e8a93d3d6f49acc
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 11:12:59 +0200

    icon index file

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+import Broom from '$components/icons/Broom.svelte';
+import Play from '$components/icons/Play.svelte';
+import Github from '$components/icons/Github.svelte';
+import Youtube from '$components/icons/Youtube.svelte';
+
+export { Broom, Play, Github, Youtube };
