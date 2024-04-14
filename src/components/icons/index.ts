41cd78c63a813c6f0b0e7991601183c5e5f3e5dd
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 14:10:41 +0200

    added stopwatch and python

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,5 +2,7 @@ import Broom from '$components/icons/Broom.svelte';
 import Play from '$components/icons/Play.svelte';
 import Github from '$components/icons/Github.svelte';
 import Youtube from '$components/icons/Youtube.svelte';
+import Python from '$components/icons/Python.svelte';
+import Stopwatch from '$components/icons/Stopwatch.svelte';
 
-export { Broom, Play, Github, Youtube };
+export { Broom, Play, Github, Youtube, Python, Stopwatch };
