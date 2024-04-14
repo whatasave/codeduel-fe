6690d03c3a3407cfc5713bb033d3ccb645bb6df4
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 14:12:01 +0200

    added danger variant

--- src/components/button/Button.svelte
+++ src/components/button/Button.svelte
@@ -6,9 +6,10 @@
 	const classes = cva('w-fit h-fit', {
 		variants: {
 			variant: {
-				primary: 'px-8 py-2 bg-[#151515] text-white rounded-sm text-[1.25rem] font-semibold',
-				accent: 'px-8 py-2 bg-[#ECC206] rounded-sm text-black text-[1.25rem] font-semibold',
-				empty: 'px-8 py-2 border-2 text-white border-white rounded-sm text-[1.25rem] font-semibold'
+				primary: 'px-6 py-1 bg-[#151515] text-white rounded-sm font-semibold',
+				accent: 'px-6 py-1 bg-[#ECC206] rounded-sm text-black font-semibold',
+				empty: 'px-6 py-1 border-2 text-white border-white rounded-sm font-semibold',
+				danger: 'px-6 py-1 bg-red-500 text-white rounded-sm font-semibold'
 			}
 		}
 	});
