c3c356754ebbe7b3d492034dfe7cd786f7c54811
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 12:03:41 +0200

    Refactor Select component and add Input and Select components to the repository

---
+++ src/components/input/Input.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { cva, type VariantProps } from 'class-variance-authority';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	const classes = cva(
+		[
+			'outline-none border-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
+			'w-fit h-fit min-w-0'
+		],
+		{
+			variants: {
+				variant: {
+					primary: 'bg-[#151515] rounded-sm px-2 py-1'
+				}
+			}
+		}
+	);
+
+	interface $$Props extends HTMLInputAttributes, VariantProps<typeof classes> {}
+
+	let { variant = 'primary', class: customClass, ...props }: $$Props = $props();
+</script>
+
+<input {...props} class={classes({ variant, className: customClass })} />
---
+++ src/components/input/Select.svelte
@@ -0,0 +1,48 @@
+<script lang="ts" generics="T">
+	import type { HTMLSelectAttributes } from 'svelte/elements';
+
+	import { cva, type VariantProps } from 'class-variance-authority';
+
+	// cursor-pointer px-1 py-1
+	const classes = cva(
+		[
+			'outline-none border-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
+			'w-fit h-fit min-w-0 cursor-pointer'
+		],
+		{
+			variants: {
+				variant: {
+					primary: 'bg-[#151515] rounded-sm px-2 py-1'
+				}
+			}
+		}
+	);
+
+	interface $$Props extends HTMLSelectAttributes, VariantProps<typeof classes> {
+		options: T[];
+		selectedIndex: number;
+		mapToString: (option: T) => string;
+		onselect?: (this: HTMLSelectElement) => void;
+	}
+
+	let {
+		options,
+		selectedIndex = $bindable(),
+		mapToString,
+		onselect,
+		variant = 'primary',
+		class: customClass,
+		...props
+	}: $$Props = $props();
+
+	function onchange(this: HTMLSelectElement) {
+		selectedIndex = parseInt(this.value);
+		onselect?.call(this);
+	}
+</script>
+
+<select {...props} class={classes({ variant, className: customClass })} {onchange}>
+	{#each options as option, i}
+		<option value={i} selected={i === selectedIndex}>{mapToString(option)}</option>
+	{/each}
+</select>
