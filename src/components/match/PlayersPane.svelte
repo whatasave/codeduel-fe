176281895201a53e2fddc49b60e9d5b531599475
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 14:12:32 +0200

    added badges to players in the match

--- src/components/match/PlayersPane.svelte
+++ src/components/match/PlayersPane.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Pane from '$components/Pane.svelte';
+	import Python from '$components/icons/Python.svelte';
 	import type { User, UserId } from '$lib/types';
 	import PlayerCircle from './PlayerCircle.svelte';
 
@@ -8,6 +9,6 @@
 
 <Pane class="flex flex-col gap-2 p-2">
 	{#each Object.entries(players) as [_, player]}
-		<PlayerCircle {player} />
+		<PlayerCircle variant="status" status="success" badge={Python} badgeBackground="#1D74C5" {player} />
 	{/each}
 </Pane>
