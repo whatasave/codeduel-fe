4bddf301ad7e414d9a66097e6027efaf5fe8172b
Kaj Rusilowski <xedom08@gmail.com>
2024-04-14 16:42:20 +0200

    added upload icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,9 +1,10 @@
 import Broom from '$components/icons/Broom.svelte';
-import Play from '$components/icons/Play.svelte';
 import Github from '$components/icons/Github.svelte';
-import Youtube from '$components/icons/Youtube.svelte';
+import Play from '$components/icons/Play.svelte';
+import ProfileDefault from '$components/icons/ProfileDefault.svelte';
 import Python from '$components/icons/Python.svelte';
 import Stopwatch from '$components/icons/Stopwatch.svelte';
-import ProfileDefault from '$components/icons/ProfileDefault.svelte';
+import Upload from '$components/icons/Upload.svelte';
+import Youtube from '$components/icons/Youtube.svelte';
 
-export { Broom, Play, Github, Youtube, Python, Stopwatch, ProfileDefault };
+export { Broom, Github, Play, ProfileDefault, Python, Stopwatch, Upload, Youtube };
