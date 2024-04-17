f410f3bcfba34505fa89addf689a91d97928ec04
Kaj Rusilowski <xedom08@gmail.com>
2024-04-17 22:52:27 +0200

    update buttons

--- src/components/button/ButtonIconLink.svelte
+++ src/components/button/ButtonIconLink.svelte
@@ -1,25 +1,24 @@
 <script lang="ts">
-	import type { ComponentProps, ComponentType, SvelteComponent } from 'svelte';
-	import Clickable from './Clickable.svelte';
+	import type { ComponentType, SvelteComponent } from 'svelte';
 	import { type VariantProps } from 'class-variance-authority';
 	import { Loading } from '$components/icons';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
 	import { ButtonStyles } from './ButtonStyles';
 
-	type IconComp = {
-		icon: ComponentType<SvelteComponent>;
-		class?: string;
-		align?: 'left' | 'right';
-		fill?: string;
-	};
-	interface $$Props extends Omit<ComponentProps<Clickable>, 'children'>, VariantProps<typeof ButtonStyles> {
-		icon: IconComp;
+	interface $$Props extends HTMLAnchorAttributes, VariantProps<typeof ButtonStyles> {
+		icon: {
+			icon: ComponentType<SvelteComponent>;
+			class?: string;
+			align?: 'left' | 'right';
+			fill?: string;
+		};
 		text?: string;
 	}
 
 	let { text, class: className, variant, icon, ...props }: $$Props = $props();
 </script>
 
-<Clickable {...props} class={ButtonStyles({ variant, className })}>
+<a {...props} class={ButtonStyles({ variant, className })}>
 	<div class="flex items-center gap-2" class:flex-row-reverse={icon.align === 'left'}>
 		{#if text}
 			<span>{text}</span>
@@ -29,4 +28,4 @@
 	{#snippet loading()}
 		<Loading fill="#ffffff" />
 	{/snippet}
-</Clickable>
+</a>
---
+++ src/components/button/ButtonLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import { ButtonStyles } from './ButtonStyles';
+	import type { VariantProps } from 'class-variance-authority';
+
+	interface $$Props extends HTMLAnchorAttributes, VariantProps<typeof ButtonStyles> {
+		text: string;
+	}
+
+	let { text, class: className, variant, ...props }: $$Props = $props();
+</script>
+
+<a {...props} class={ButtonStyles({ variant, className })}>
+	{text}
+</a>
