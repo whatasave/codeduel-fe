a1d2c0dac62d09d1d3917944bddae014e8bbd8e6
Filippo Monteleone <136260618+filippo-monteleone@users.noreply.github.com>
2024-04-18 22:38:19 +0200

    user interface (#11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,svelte}"],
-  theme: {
-    extend: {},
-    borderRadius: {
-      DEFAULT: '0.5rem',
-      sm: '0.25rem',
-      full: '50%'
-    }
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,svelte}'],
+	theme: {
+		extend: {
+			colors: {}
+		},
+		borderRadius: {
+			DEFAULT: '0.5rem',
+			sm: '0.25rem',
+			full: '50%'
+		}
+	},
+	plugins: []
+};
