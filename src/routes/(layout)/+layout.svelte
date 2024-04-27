739a24fa2ecf16555c1861fcbc3de962c3e2935e
Kaj Rusilowski <xedom08@gmail.com>
2024-04-27 13:29:56 +0200

    updated svelte5

--- src/routes/(layout)/+layout.svelte
+++ src/routes/(layout)/+layout.svelte
@@ -11,7 +11,7 @@
 <div class="min-h-0 h-full flex flex-col p-2 gap-2">
 	<Navbar user={data.user} />
 
-	<main class="flex-1 flex flex-col py-2 overflow-y-auto">
+	<main class="flex-1 flex flex-col overflow-y-auto">
 		{@render children()}
 	</main>
 
