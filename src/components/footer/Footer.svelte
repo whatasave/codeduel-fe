8cb50a6265073b2721679960e6507364951ee3ea
Kaj Rusilowski <xedom08@gmail.com>
2024-05-06 22:46:11 +0200

    fix icons

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -2,11 +2,11 @@
 	import { Github, Youtube } from '$components/icons';
 </script>
 
-<footer class="rounded bg-white/5 px-6 py-4 text-xs">
+<footer class="rounded bg-white/5 px-6 py-4 text-xs text-white/70">
 	<div class="mx-auto flex w-full max-w-[1400px] flex-row-reverse justify-between gap-4">
 		<nav class="flex items-center justify-center gap-4">
-			<a href="https://youtu.be/AIGnRc7F86Q" target="_blank"><Youtube /></a>
-			<a href="https://github.com/whatasave" target="_blank"><Github /></a>
+			<a href="https://youtu.be/AIGnRc7F86Q" target="_blank"><Youtube class="opacity-70" /></a>
+			<a href="https://github.com/whatasave" target="_blank"><Github class="opacity-70" /></a>
 		</nav>
 		<nav class="flex items-center justify-center gap-4">
 			<span>© 2024 CodeDuel.</span>
