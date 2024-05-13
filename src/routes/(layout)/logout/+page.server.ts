3f00336535f0f948e055c5b890037e2adbfbd388
Kaj Rusilowski <xedom08@gmail.com>
2024-05-14 01:43:39 +0200

    api update

--- src/routes/(layout)/logout/+page.server.ts
+++ src/routes/(layout)/logout/+page.server.ts
@@ -3,5 +3,5 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({ cookies }) {
 	cookies.set('jwt', '', { path: '/', maxAge: -1, secure: true });
-	redirect(301, `${PUBLIC_BACKEND_URL}/v1/logout`);
+	redirect(301, `${PUBLIC_BACKEND_URL}/v1/auth/logout`);
 }
