10dc2ef79e99ca3bbf8225770cc998f9fcd6a559
Kaj Rusilowski <xedom08@gmail.com>
2024-05-14 02:01:31 +0200

    fix

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -14,7 +14,7 @@ class Backend {
 	}
 
 	private getCookie(name: string): string | null {
-		if (browser) return null;
+		if (!browser) return null;
 		const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
 		if (match) return match[2];
 		return null;
