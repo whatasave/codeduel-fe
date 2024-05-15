6f4606b6ff4abbe87d80af9ce9099cd3e4e2a4a5
Kaj Rusilowski <xedom08@gmail.com>
2024-05-15 11:23:58 +0200

    made footer responsive

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <footer class="rounded bg-white/5 px-6 py-4 text-xs text-white/70">
-	<div class="mx-auto flex w-full max-w-[1400px] flex-row-reverse justify-between gap-4">
+	<div class="mx-auto flex w-full max-w-[1400px] flex-row-reverse flex-wrap justify-between gap-4 max-[600px]:flex-col">
 		<nav class="flex items-center justify-center gap-4">
 			<a href="https://youtu.be/AIGnRc7F86Q" target="_blank"><Youtube class="opacity-70" /></a>
 			<a href="https://github.com/whatasave" target="_blank"><Github class="opacity-70" /></a>
 		</nav>
-		<nav class="flex items-center justify-center gap-4">
-			<span>© 2024 CodeDuel.</span>
+		<nav class="flex flex-wrap items-center justify-center gap-4">
+			<span>© 2024 Codeduel</span>
 			<a href="#privacy">Privacy</a>
 			<a href="#terms">Terms</a>
 			<a href="#contact">Contact</a>
