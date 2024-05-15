5e0c1a8c769cd478f60776a3a714a2cf39b75dde
Kaj Rusilowski <xedom08@gmail.com>
2024-05-15 11:26:55 +0200

    fix strange error

--- src/routes/(layout)/+layout.svelte
+++ src/routes/(layout)/+layout.svelte
@@ -5,11 +5,11 @@
 	import type { PageData } from './$types';
 
 	let { data, children }: { data: PageData, children: Snippet } = $props();
-	
+
 </script>
 
 <div class="min-h-0 h-full flex flex-col p-2 gap-2">
-	<Navbar user={data.user} />
+	<Navbar user={data && data.user} />
 
 	<main class="flex-1 flex flex-col overflow-y-auto">
 		{@render children()}
