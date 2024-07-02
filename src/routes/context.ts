39dd64366a8c66187fd9948f3b3e3d1a38b8ce99
Michi Palazzo <palazzomichi@gmail.com>
2024-07-03 01:14:04 +0200

    Update

--- src/routes/context.ts
+++ src/routes/context.ts
@@ -1,9 +1,9 @@
 import type { UserProfile } from '$lib/types';
 import { getContext, setContext } from 'svelte';
 
-function context<T>() {
-	const key = Symbol();
+function context<T>(description?: string) {
+	const key = Symbol(description);
 	return [() => getContext<T>(key), (value: T) => setContext(key, value)] as const;
 }
 
-export const [getUser, setUser] = context<Promise<UserProfile | null>>();
+export const [getUser, setUser] = context<Promise<UserProfile | null>>("USER");
