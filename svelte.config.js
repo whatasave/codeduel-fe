4c0ed9ef1d6d6d44997dc31382fb550d812d9c3b
Michi Palazzo <palazzomichi@gmail.com>
2025-02-23 20:06:38 +0100

    Update svelte

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapterAuto from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-function adapter() {
-	if (process.env.SVELTEKIT_ADAPTER === 'node') return adapterNode();
-	if (process.env.SVELTEKIT_ADAPTER === 'auto') return adapterAuto();
+function adapter(options) {
+	if (process.env.SVELTEKIT_ADAPTER === 'node') return adapterNode(options);
+	if (process.env.SVELTEKIT_ADAPTER === 'auto') return adapterAuto(options);
 
 	console.warn(
 		'No adapter specified. Using `adapter-auto` by default. Specify `ADAPTER=node` or `ADAPTER=auto` to use a different adapter.'
 	);
-	return adapterAuto();
+	return adapterAuto(options);
 }
 
 /** @type {import('@sveltejs/kit').Config} */
